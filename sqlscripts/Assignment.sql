82563679be5bd4d923085762eada0dffea871581
Zakariyya Uddin <z.uddin@kainos.com>
2020-11-12 16:25:03 +0000

    Added foreign keys from Assignment to EmployeeID and ProjectID.

--- sqlscripts/Assignment.sql
+++ sqlscripts/Assignment.sql
@@ -2,5 +2,7 @@ CREATE TABLE Assignment (
     ProjectID MEDIUMINT NOT NULL,
     EmployeeID MEDIUMINT NOT NULL,
     StartDate DATETIME NOT NULL DEFAULT NOW(),
-    PRIMARY KEY (ProjectID, EmployeeID, StartDate)
+    PRIMARY KEY (ProjectID, EmployeeID, StartDate),
+    FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID),
+    FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
 );
