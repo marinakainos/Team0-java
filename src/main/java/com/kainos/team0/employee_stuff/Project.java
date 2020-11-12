65850691f3d2157cd05ef45014e1e71fcf859317
Kieran <k.broomhall@kainos.com>
2020-11-12 16:08:26 +0000

    Add the blank template for project and project controller

---
+++ src/main/java/com/kainos/team0/employee_stuff/Project.java
@@ -0,0 +1,6 @@
+package com.kainos.team0.employee_stuff;
+
+public class Project {
+
+    private int 
+}
---
+++ src/main/java/com/kainos/team0/employee_stuff/ProjectController.java
@@ -0,0 +1,4 @@
+package com.kainos.team0.employee_stuff;
+
+public class ProjectController {
+}
