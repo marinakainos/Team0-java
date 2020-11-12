23b94d6f11bb8c7131e22fc9b4ecb715036875ea
Kieran <k.broomhall@kainos.com>
2020-11-12 16:39:33 +0000

    Add Project class for project objects to be created

--- src/main/java/com/kainos/team0/employee_stuff/Project.java
+++ src/main/java/com/kainos/team0/employee_stuff/Project.java
@@ -2,5 +2,28 @@ package com.kainos.team0.employee_stuff;
 
 public class Project {
 
-    private int 
+    private int id;
+    private String name;
+
+
+    public Project(int id, String name){
+        this.setId(id);
+        this.setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
