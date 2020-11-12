acf80fa5c084a18f46fce4c2ccd7e269b91f5e78
Your Name <your.name@kainos.com>
2020-11-12 14:26:37 +0000

    created department class

---
+++ src/main/java/com/kainos/team0/employee_stuff/Department.java
@@ -0,0 +1,11 @@
+package com.kainos.team0.employee_stuff;
+
+public class Department {
+
+
+    public Department(String departmentName) {
+        this.name = departmentName;
+    }
+
+    private string departmentName;
+}
