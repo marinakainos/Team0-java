ec9dded1129dea0a27ce701dd81b272c92fb76ca
Zakariyya Uddin <z.uddin@kainos.com>
2020-11-12 11:54:49 +0000

    Added Employee create table and Employee Insert statements.

---
+++ sqlscripts/Employee.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Employee (
+    EmployeeID MEDIUMINT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(100) NOT NULL,
+    Address VARCHAR(200) NOT NULL,
+    NI CHAR(9) NOT NULL,
+    IBAN CHAR(34),
+    BIC VARCHAR(11),
+    Salary INT NOT NULL CHECK (Salary >= 0),
+    EmployeeNumber CHAR(8) NOT NULL,
+    UNIQUE(NI)
+);
---
+++ sqlscripts/EmployeeData.sql
@@ -0,0 +1,23 @@
+INSERT INTO Employee (EmployeeID, EmployeeNumber, Name, Address, NI, IBAN, BIC, Salary)
+VALUES (1, 'ABC123', 'Adam Crawley', '55 Bristol Road', 'AB123456C', 'SE3550000000054910000003', null, 350000);
+
+INSERT INTO Employee (EmployeeID, EmployeeNumber, Name, Address, NI, IBAN, BIC, Salary)
+VALUES (2, 'DEF456', 'Daniel Osbourne', '199 Maine Road', 'DE789456F', 'CH9300762011623852957', null, 190000);
+
+INSERT INTO Employee (EmployeeID, EmployeeNumber, Name, Address, NI, IBAN, BIC, Salary)
+VALUES (3, 'GHI789', 'George Hardy', '11 Baker Street', 'GH789123I', 'DE89370400440532013000', null, 230000);
+
+INSERT INTO Employee (EmployeeID, EmployeeNumber, Name, Address, NI, IBAN, BIC, Salary)
+VALUES (4, 'JKL789', 'Julius Sandy', '63 Liverpool Street', 'JK789442L', null, 'BNPAFRPPKML', 450000);
+
+INSERT INTO Employee (EmployeeID, EmployeeNumber, Name, Address, NI, IBAN, BIC, Salary)
+VALUES (5, 'DSA789', 'Ralph Darren', '100 Trafford Way', 'OT789552A', null, 'DEUTDEFFHJA', 1500000);
+
+INSERT INTO Employee (EmployeeID, EmployeeNumber, Name, Address, NI, IBAN, BIC, Salary)
+VALUES (6, 'FOL943', 'Fiona Lauren', '77 Stratford Road', 'FM789552X', null, 'BARTDEFFZZH', 2500000);
+
+INSERT INTO Employee (EmployeeID, EmployeeNumber, Name, Address, NI, IBAN, BIC, Salary)
+VALUES (7, 'JJM634', 'Janet Mary', '40 Dolphin Lane', 'JJ772552M', null, 'CCPAFRPSKML', 1150000);
+
+INSERT INTO Employee (EmployeeID, EmployeeNumber, Name, Address, NI, IBAN, BIC, Salary)
+VALUES (8, 'PAW329', 'Paula Gerald', '19 Lakey Lane', 'PA789412A', 'SS89370405540532013111', null, 450000);
