d4ffe7c3c97dcda750b57436de764290a8685252
Adam Cox <a.cox@kainos.com>
2020-11-12 16:15:55 +0000

    Add CreateProject method

--- src/main/java/com/kainos/team0/employee_stuff/ProjectController.java
+++ src/main/java/com/kainos/team0/employee_stuff/ProjectController.java
@@ -1,4 +1,34 @@
 package com.kainos.team0.employee_stuff;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ProjectController {
+
+    private Connection connection;
+
+    public ProjectController(Connection connection) {
+        this.connection = connection;
+    }
+
+    public String CreateProject(String projectName) {
+        String insertString = "INSERT INTO Project(ProjectName) VALUES(?)";
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement(insertString);
+
+            preparedStatement.setString(1, projectName);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected == 1) {
+                return projectName;
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
 }
+
